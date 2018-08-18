5e0d1802ecc198156ebb2115cb4b646c13b55ed4
Tanakancolle <tanakancolle@gmail.com>
2018-08-18 17:48:20 +0900

    add Resources Load Support

---
+++ Assets/ResourcesSupport/Editor/Scripts/Parameters/LoadParameter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine.Serialization;
+
+namespace TemplateEditor
+{
+    public class LoadParameter : ScriptableObject, IUsings
+    {
+        public string TypeName;
+
+        [EnumFlags]
+        public ResourcesLoaderUtility.LoadType EditLoadType = ResourcesLoaderUtility.LoadType.Load;
+
+        public string[] TargetExtensions;
+
+        public string[] EditUsings;
+        public string[] usings { get { return EditUsings; } }
+    }
+}
