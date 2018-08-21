69ce40791eaf1636ea8e6eed806a501f7ed01dab
Tanakancolle <tanakancolle@gmail.com>
2018-08-21 18:17:26 +0900

    ReleaseExecutor追加

---
+++ Assets/ReleaseExecutor/Editor/PackageExporter.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+namespace ReleaseExecutor
+{
+    public class PackageExporter : EditorWindow
+    {
+        public static void Open()
+        {
+            GetWindow<PackageExporter>(true);
+        }
+
+        private PackageExporterSetting _exportSetting;
+        private Editor _editor;
+
+        void OnGUI()
+        {
+            var newSetting = EditorGUILayout.ObjectField(_exportSetting, typeof(PackageExporterSetting), false) as PackageExporterSetting;
+            if (newSetting == null)
+            {
+                return;
+            }
+
+            if (newSetting != _exportSetting)
+            {
+                _exportSetting = newSetting;
+                _editor = Editor.CreateEditor(_exportSetting);
+            }
+
+            _editor.OnInspectorGUI();
+
+            if (GUILayout.Button("エクスポート"))
+            {
+                RunExpotPackage(_exportSetting);
+            }
+        }
+
+        public static void RunExpotPackage(PackageExporterSetting setting)
+        {
+            ExportPackage(setting.TargetPaths, setting.ExportName, setting.Options);
+        }
+
+        private static void ExportPackage(string[] files, string exportName, ExportPackageOptions[] optionsArray)
+        {
+            if (!CheckFilesExist(files))
+            {
+                return;
+            }
+
+            var exportFullName = AddExportExtension(exportName);
+
+            ExportPackageOptions exportOptions = ExportPackageOptions.Default;
+            foreach (var options in optionsArray)
+            {
+                exportOptions |= options;
+            }
+
+            AssetDatabase.ExportPackage(files, exportFullName, exportOptions);
+            Debug.LogFormat("Exported to: {0}\nExported: {1}", exportFullName, string.Join("\n", files));
+        }
+
+        private static string[] AddAssetPrefix(string[] strings)
+        {
+            var paths = new string[strings.Length];
+            for (int i = 0; i < strings.Length; i++)
+            {
+                paths[i] = "Assets/" + strings[i];
+            }
+
+            return paths;
+        }
+
+        public static string AddExportExtension(string exportName)
+        {
+            return exportName + ".unitypackage";
+        }
+
+        private static bool CheckFilesExist(string[] files)
+        {
+            var root = Application.dataPath;
+
+            foreach (var file in files)
+            {
+                var path = Path.Combine(root, file);
+
+                if (!File.Exists(path) && !Directory.Exists(path))
+                {
+                    Debug.LogErrorFormat("'{0}' doesn't exist!", file);
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Assets/ReleaseExecutor/Editor/ReleaseExecutorMenuItem.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using System.IO;
+
+namespace ReleaseExecutor
+{
+    public class ReleaseExecutorMenuItem
+    {
+        private enum Priority
+        {
+            CreateSetting,
+            ExportWindow = 20,
+            ExportAllPackage,
+        }
+
+        private const string MenuItemPrefix = "Tools/Release Executor/";
+        private const int OriginalPriorityNumber = 1000;
+
+
+        [MenuItem(MenuItemPrefix + "Create Setting", false, OriginalPriorityNumber + (int)Priority.CreateSetting)]
+        public static void CreateSetting()
+        {
+            CreateScriptableObject<PackageExporterSetting>();
+        }
+
+        [MenuItem(MenuItemPrefix + "Export Package Window", false, OriginalPriorityNumber + (int)Priority.ExportWindow)]
+        public static void OpenExportPackageWindow()
+        {
+            PackageExporter.Open();
+        }
+
+        [MenuItem(MenuItemPrefix + "Export Package All", false, OriginalPriorityNumber + (int)Priority.ExportAllPackage)]
+        public static void ExportAllPackage()
+        {
+            var guids = FindAssetGuids(typeof(PackageExporterSetting));
+            foreach (var guid in guids)
+            {
+                var path = AssetDatabase.GUIDToAssetPath(guid);
+                PackageExporter.RunExpotPackage(AssetDatabase.LoadAssetAtPath<PackageExporterSetting>(path));
+            }
+        }
+
+        private static void CreateScriptableObject<T>() where T : ScriptableObject
+        {
+            var obj = ScriptableObject.CreateInstance<T>();
+            var path = Path.Combine(GetActiveFolder(), obj.name);
+            ProjectWindowUtil.CreateAsset(obj, GetCreateScriptableObjectPath<T>(path));
+            AssetDatabase.Refresh();
+        }
+
+        private static string GetCreateScriptableObjectPath<T>(string dir)
+        {
+            var pathWithoutExtension = Path.Combine(dir, typeof(T).Name);
+            var path = string.Format("{0}.asset", pathWithoutExtension);
+            return AssetDatabase.GenerateUniqueAssetPath (path);
+        }
+
+        private static string GetActiveFolder()
+        {
+            if (Selection.activeObject == null)
+            {
+                return "Assets";
+            }
+
+            var activeObjectPath = AssetDatabase.GetAssetPath(Selection.activeObject);
+            if (AssetDatabase.IsValidFolder(activeObjectPath))
+            {
+                return activeObjectPath;
+            }
+
+            return Directory.GetParent(activeObjectPath).ToString();
+        }
+
+        private static string[] FindAssetGuids(Type type)
+        {
+            return AssetDatabase.FindAssets("t:" + type.Name);
+        }
+    }
+}
