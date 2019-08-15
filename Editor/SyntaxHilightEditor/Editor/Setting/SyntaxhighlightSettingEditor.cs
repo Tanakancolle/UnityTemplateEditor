7710e9e046554db5d6a5d3f38a3bef794835356c
Shinogi <shinogi_kazuki@cyberagent.co.jp>
2019-08-15 17:13:46 +0900

    SyntaxEditor取り込み

---
+++ Editor/SyntaxHilightEditor/Editor/Setting/SyntaxhighlightSettingEditor.cs
@@ -0,0 +1,125 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System;
+
+namespace SyntaxHighlightEditor
+{
+    [CustomEditor(typeof(SyntaxHighlightSetting))]
+    public class SyntaxhighlightSettingEditor : Editor
+    {
+        /// <summary>
+        /// Property.
+        /// SyntaxhighlightSetting のメンバ名と同じにする
+        /// </summary>
+        public enum Property
+        {
+            Format,
+            FormatTypeText,
+            Color,
+            Patterns
+        }
+
+        private enum Format
+        {
+            Custom = -1,
+            Normal,
+            Delimit,
+        }
+
+        private static readonly string[] Formats =
+        {
+            "(?<{0}>({1}))",
+            "(?<{0}>(?<![0-9a-zA-Z_])({1})(?![0-9a-zA-Z_]))"
+        };
+
+        private Color _highlightColor = Color.white;
+        private string _customFormat;
+        private SerializedProperty[] _properties;
+        private Format _formatType;
+
+        private void OnEnable()
+        {
+            var setting = target as SyntaxHighlightSetting;
+
+            var names = Enum.GetNames(typeof(Property));
+            _properties = new SerializedProperty[names.Length];
+            for (int i = 0; i < _properties.Length; ++i)
+            {
+                _properties[i] = serializedObject.FindProperty(names[i]);
+            }
+
+            ColorUtility.TryParseHtmlString("#" + setting.Color, out _highlightColor);
+            _formatType = ConvertFormatTypeFromText(setting.FormatTypeText);
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            {
+                DrawFormat();
+                _highlightColor = EditorGUILayout.ColorField("Highlight Color", _highlightColor);
+                GetProperty(Property.Color).stringValue = ColorUtility.ToHtmlStringRGBA(_highlightColor);
+                EditorGUILayout.PropertyField(GetProperty(Property.Patterns), true);
+            }
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        private SerializedProperty GetProperty(Property property)
+        {
+            return _properties[(int) property];
+        }
+
+        private void DrawFormat()
+        {
+            EditorGUILayout.BeginVertical(GUI.skin.box);
+            {
+                _formatType = (Format) EditorGUILayout.EnumPopup("Format Type", _formatType);
+                GetProperty(Property.FormatTypeText).stringValue = ConvertTextFromFormatType(_formatType);
+                switch (_formatType)
+                {
+                    case Format.Custom:
+                    {
+                        GetProperty(Property.Format).stringValue = _customFormat = EditorGUILayout.TextField(_customFormat);
+                    }
+                        break;
+
+                    default:
+                    {
+                        var result = GetFormatText(_formatType);
+                        GetProperty(Property.Format).stringValue = result;
+                        EditorGUILayout.LabelField(result);
+                    }
+                        break;
+                }
+            }
+            EditorGUILayout.EndVertical();
+        }
+
+        private Format ConvertFormatTypeFromText(string text)
+        {
+            try
+            {
+                return (Format) Enum.Parse(typeof(Format), text);
+            }
+            catch
+            {
+                return Format.Normal;
+            }
+        }
+
+        private string ConvertTextFromFormatType(Format format)
+        {
+            return Enum.GetName(typeof(Format), format);
+        }
+
+        private static string GetFormatText(Format format)
+        {
+            if (format == Format.Custom)
+            {
+                return string.Empty;
+            }
+
+            return Formats[(int) format];
+        }
+    }
+}
---
+++ Editor/SyntaxHilightEditor/Editor/TextAreaSyntaxHighlightEditor.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace SyntaxHighlightEditor
+{
+    /// <summary>
+    /// Code editor.
+    /// </summary>
+    public class TextAreaSyntaxHighlightEditor
+    {
+        public Color BackgroundColor { get; set; }
+
+        public Color TextColor { get; set; }
+
+        public System.Func<string, string> Highlighter { get; set; }
+
+        public string CachedHighlightedCode { get; set; }
+
+        private string _cachedCode;
+
+        public TextAreaSyntaxHighlightEditor()
+        {
+            BackgroundColor = Color.black;
+            TextColor = Color.white;
+            Highlighter = code => code;
+        }
+
+        public string Draw(string code, GUIStyle style, params GUILayoutOption[] options)
+        {
+            // 文字を透明にする
+            var backStyle = new GUIStyle(style);
+            SetStyleTextColor(backStyle, Color.gray);
+
+            // 現在の色を保存
+            var preBackgroundColor = GUI.backgroundColor;
+            var preColor = GUI.color;
+
+            // 編集用のテキストエリアを描画
+            GUI.backgroundColor = BackgroundColor;
+            var editedCode = EditorGUILayout.TextArea(code, backStyle, options);
+
+            // シンタックスハイライトさせたコードを更新
+            if (editedCode != _cachedCode)
+            {
+                CachedHighlightedCode = Highlighter(editedCode);
+                _cachedCode = editedCode;
+            }
+
+            // 文字（シンタックスハイライトされない部分）を指定色にする
+            var forwardStyle = new GUIStyle(style);
+            SetStyleTextColor(forwardStyle, TextColor);
+            forwardStyle.richText = true;
+
+            // シンタックスハイライト用のテキストエリアを表示
+            GUI.backgroundColor = Color.clear;
+            EditorGUI.TextArea(GUILayoutUtility.GetLastRect(), CachedHighlightedCode, forwardStyle);
+
+            // 色を元に戻す
+            GUI.backgroundColor = preBackgroundColor;
+            GUI.color = preColor;
+
+            return editedCode;
+        }
+
+        private void SetStyleTextColor(GUIStyle style, Color color)
+        {
+            style.normal.textColor = color;
+            style.hover.textColor = color;
+            style.active.textColor = color;
+        }
+    }
+}
