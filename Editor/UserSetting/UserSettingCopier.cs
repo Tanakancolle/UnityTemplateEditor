5b0e35601b16c65140fa6e5a56abf75a491a7b0e
Shinogi <shinogi_kazuki@cyberagent.co.jp>
2019-10-11 17:38:48 +0900

    ユーザー設置のコピー機能のベースを追加

---
+++ Editor/UserSetting/UserSettingCopier.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using TemplateEditor;
+using UnityEditor;
+using UnityEngine;
+
+public class UserSettingCopier
+{
+    private static readonly string DefaultTemplateDirectory = "/TemplateTool/";
+
+    [MenuItem("Test/Test2")]
+    public static void Test()
+    {
+        var setting = ToolExecutor.GetUseUserSetting();
+        Copy(setting, "Assets/TestFolder");
+    }
+
+    private static void Copy(UserSettingBase target, string copyDirectory)
+    {
+        Directory.CreateDirectory(copyDirectory);
+
+        var targetPath = AssetDatabase.GetAssetPath(target);
+        var copyPath = Path.Combine(copyDirectory, Path.GetFileName(targetPath));
+        AssetDatabase.CopyAsset(targetPath, copyPath);
+
+        var copyAsset = AssetDatabase.LoadAssetAtPath<UserSettingBase>(copyPath);
+        var copyAssetSerialized = new SerializedObject(copyAsset);
+        foreach (var property in ScriptableObjectUtility.GetSerializedProperties(copyAssetSerialized, true, true))
+        {
+            if (property.propertyType == SerializedPropertyType.ObjectReference && property.objectReferenceValue != null)
+            {
+                if (property.name == "m_Script")
+                {
+                    continue;
+                }
+
+                var childTargetPath = AssetDatabase.GetAssetPath(property.objectReferenceValue);
+                var childTargetDirectory = Path.GetDirectoryName(childTargetPath);
+                var childCopyPath = Path.Combine(
+                    copyDirectory,
+                    "UsedAssets",
+                    childTargetDirectory.Substring(childTargetDirectory.IndexOf(DefaultTemplateDirectory) + DefaultTemplateDirectory.Length),
+                    Path.GetFileName(childTargetPath));
+                Directory.CreateDirectory(Path.GetDirectoryName(childCopyPath));
+                AssetDatabase.CopyAsset(childTargetPath, childCopyPath);
+                property.objectReferenceValue = AssetDatabase.LoadAssetAtPath(childCopyPath, property.objectReferenceValue.GetType());
+            }
+        }
+
+        copyAssetSerialized.ApplyModifiedProperties();
+    }
+}
--- Editor/Utility/ScriptableObjectUtility/ScriptableObjectUtility.cs
+++ Editor/Utility/ScriptableObjectUtility/ScriptableObjectUtility.cs
@@ -7,7 +7,7 @@ namespace TemplateEditor
 {
     public static class ScriptableObjectUtility
     {
-        private static readonly string EmptyScriptableObjectGuid = "7b47cc4df51e64f73aa22432250a8be5";
+        private static ScriptableObject _tempScriptableObject;
 
         public static string[] GetSerializeNamesWithoutDefault(ScriptableObject target)
         {
@@ -17,23 +17,47 @@ namespace TemplateEditor
 
         public static List<string> GetDefaultSerializeNames()
         {
-            var empty = AssetDatabase.LoadAssetAtPath<ScriptableObject>(AssetDatabase.GUIDToAssetPath(EmptyScriptableObjectGuid));
-            return GetSerializeNames(empty);
+            if (_tempScriptableObject == null)
+            {
+                _tempScriptableObject = ScriptableObject.CreateInstance<EmptyScriptableObject>();
+            }
+
+            return GetSerializeNames(_tempScriptableObject);
         }
 
         public static List<string> GetSerializeNames(ScriptableObject target)
         {
             var list = new List<string>();
-            var serializedObject = new SerializedObject(target);
-            var property = serializedObject.GetIterator();
-            property.Next(true);
-            list.Add(property.name);
-            while (property.Next(false))
+            foreach (var property in GetSerializedProperties(target))
             {
                 list.Add(property.name);
             }
 
             return list;
+        }
+
+        public static IEnumerable<SerializedProperty> GetSerializedProperties(ScriptableObject target, bool isChildren = false, bool isApplyModified = false)
+        {
+            var serializedObject = new SerializedObject(target);
+            return GetSerializedProperties(serializedObject, isChildren, isApplyModified);
+        }
+
+        public static IEnumerable<SerializedProperty> GetSerializedProperties(SerializedObject serializedObject, bool isChildren = false, bool isApplyModified = false)
+        {
+            var property = serializedObject.GetIterator();
+            property.Next(true);
+
+            yield return property;
+
+            while (property.Next(isChildren))
+            {
+                yield return property;
+            }
+
+            if (isApplyModified)
+            {
+                serializedObject.ApplyModifiedProperties();
+            }
         }
     }
 }
